2607e07167cc45a08fd6d722c03b4900b3dc2d79
Bobby BuffaloBoy <novenarii@gmail.com>
2026-02-23 13:35:07 +0100

    fixed lint issues

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000'
+    origins "http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3000"
 
-    resource '*', 
+    resource "*",
     headers: :any,
-    methods: [:get, :post, :put, :patch, :delete, :options, :head],
-    expose: ['Authorization'],
+    methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+    expose: [ "Authorization" ],
     max_age: 600
   end
 end
